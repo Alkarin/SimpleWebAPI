fc11ec2ffa803cbc23e72733f1d5f839c7decb16
AlexanderV <avaughan@barefootsolutions.com>
2018-01-19 14:35:15 -0800

    Move RetrieveFeedTask to own class file

---
+++ SimpleWebAPI/src/main/java/com/sample/foo/simplewebapi/RetrieveFeedTask.java
@@ -0,0 +1,92 @@
+package com.sample.foo.simplewebapi;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static com.sample.foo.simplewebapi.MainActivity.API_KEY;
+import static com.sample.foo.simplewebapi.MainActivity.API_URL;
+
+/**
+ * Created by alexander on 1/19/18.
+ */
+
+
+public class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
+
+    private TextView responseView;
+    private EditText emailText;
+    private ProgressBar progressBar;
+
+    RetrieveFeedTask(TextView responseView, EditText emailText, ProgressBar progressBar){
+        this.responseView = responseView;
+        this.emailText = emailText;
+        this.progressBar = progressBar;
+    }
+
+    private Exception exception;
+
+    protected void onPreExecute() {
+        progressBar.setVisibility(View.VISIBLE);
+        responseView.setText("");
+    }
+
+    protected String doInBackground(Void... urls) {
+        String email = emailText.getText().toString();
+        // Do some validation here
+
+        try {
+            URL url = new URL(API_URL + "email=" + email + "&apiKey=" + API_KEY);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                bufferedReader.close();
+                return stringBuilder.toString();
+            }
+            finally{
+                urlConnection.disconnect();
+            }
+        }
+        catch(Exception e) {
+            Log.e("ERROR", e.getMessage(), e);
+            return null;
+        }
+    }
+
+    protected void onPostExecute(String response) {
+        if(response == null) {
+            response = "THERE WAS AN ERROR";
+        }
+        progressBar.setVisibility(View.GONE);
+        Log.i("INFO", response);
+        responseView.setText(response);
+        // TODO: check this.exception
+        // TODO: do something with the feed
+
+//            try {
+//                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+//                String requestID = object.getString("requestId");
+//                int likelihood = object.getInt("likelihood");
+//                JSONArray photos = object.getJSONArray("photos");
+//                .
+//                .
+//                .
+//                .
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+    }
+}
